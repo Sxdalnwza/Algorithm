2bf4a78bac4833f1a64df3ed3192327e8e6e7e1a
“Sxdalnwza” <paweekorn2006@gmail.com>
2025-09-05 14:50:46 +0700

    first commit

--- Soda.java
+++ Soda.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Soda {
     public static void main(String args[]) {
-        // int money 
-        // int tax 
+        // int money
+        // int tax
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter money " );
+        System.out.print("Enter money ");
         int money = scanner.nextInt();
-        System.out.print("Enter tax " );
+        System.out.print("Enter tax ");
         int tax = scanner.nextInt();
         int total = money * tax;
         System.out.println("total is = " + total);
